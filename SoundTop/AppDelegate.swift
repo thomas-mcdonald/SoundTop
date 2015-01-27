b6e7fe3cfc6ce221ec03b82b0262172e6f2e4459
Thomas McDonald <tom@conceptcoding.co.uk>
2015-01-27 15:16:58 +0000

    Initial Commit

---
+++ SoundTop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoundTop
+//
+//  Created by Thomas McDonald on 27/01/2015.
+//  Copyright (c) 2015 Thomas McDonald. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundTop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundTop
+//
+//  Created by Thomas McDonald on 27/01/2015.
+//  Copyright (c) 2015 Thomas McDonald. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
