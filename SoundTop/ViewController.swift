bd780774cdfc65429784fb70cad815860502fe34
Thomas McDonald <tom@conceptcoding.co.uk>
2015-01-28 16:36:35 +0000

    animate visual effect view on window hover

--- SoundTop/ViewController.swift
+++ SoundTop/ViewController.swift
@@ -10,10 +10,11 @@ import Cocoa
 
 class ViewController: NSViewController {
     @IBOutlet weak var albumView : STAlbumView!
+    @IBOutlet weak var songVisualEffectView : NSVisualEffectView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        songVisualEffectView.alphaValue = 0
     }
 
     override var representedObject: AnyObject? {
@@ -22,12 +23,29 @@ class ViewController: NSViewController {
         }
     }
 
-    override func mouseEntered(theEvent: NSEvent) {
-        NSLog("Mouse entered")
+    override func mouseEntered(e: NSEvent) {
+        songVisualEffectView.alphaValue = 1
     }
 
-    override func mouseExited(theEvent: NSEvent) {
-        NSLog("Mouse exited")
+    override func mouseExited(e: NSEvent) {
+        delay(1.0) {
+            // needs improving to add removal eg where user re-enters area
+            NSAnimationContext.beginGrouping()
+            NSAnimationContext.currentContext().duration = 0.5
+            self.songVisualEffectView.animator().alphaValue = 0
+            NSAnimationContext.endGrouping()
+        }
     }
+
+    // http://stackoverflow.com/questions/24034544/dispatch-after-gcd-in-swift/24318861#24318861
+    private func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+
 }
 
