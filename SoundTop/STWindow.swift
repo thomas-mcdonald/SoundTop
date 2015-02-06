3d5a64a2e81bb561dad2d7c555644912167d6c8d
Thomas McDonald <tom@conceptcoding.co.uk>
2015-02-06 18:18:28 +0000

    dragging on the albumview should move the window around

--- SoundTop/STWindow.swift
+++ SoundTop/STWindow.swift
@@ -1,4 +1,4 @@
-// STAlbumView.swift
+// STWindow.swift
 //
 // Copyright (c) 2015 Thomas McDonald
 //
@@ -27,49 +27,8 @@
 
 import Cocoa
 
-class STAlbumView: NSImageView {
-    var trackingArea : NSTrackingArea?
-
-    override var mouseDownCanMoveWindow: Bool {
+class STWindow: NSWindow {
+    var isMovableByWindowBackground: Bool {
         get { return true }
-    }
-    
-    override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect);
-        setup()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder);
-        setup()
-    }
-    
-    func setup() {
-        let url = NSURL(string: "https://i1.sndcdn.com/artworks-000092147250-ei0cxn-t500x500.jpg")!
-        self.fetchImage(url);
-    }
-
-    func fetchImage(url : NSURL) {
-        let request = NSURLRequest(URL: url) // TODO: unwrap optional properly
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
-            if error == nil {
-                let image : NSImage = NSImage(data: data)!
-                self.image = image
-                self.needsDisplay = true
-            }
-            else {
-                println("Error: \(error.localizedDescription)")
-            }
-        })
-    }
-
-    override func updateTrackingAreas() {
-        super.updateTrackingAreas()
-        if(trackingArea != nil) {
-            removeTrackingArea(trackingArea!)
-        }
-        let opts = NSTrackingAreaOptions.MouseEnteredAndExited | NSTrackingAreaOptions.ActiveAlways
-        trackingArea = NSTrackingArea.init(rect: self.bounds, options: opts, owner: nextResponder, userInfo: nil)
-        addTrackingArea(trackingArea!)
     }
 }
